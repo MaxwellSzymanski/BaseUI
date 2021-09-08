73af17eb228e4733db8db7eb798d318e565c6e4b
Maxwell <maxwell.szymanski@student.kuleuven.be>
2021-09-08 19:41:30 +0200

    Updated to Meteor 2.3.6

--- imports/ui/components/TextInput.tsx
+++ imports/ui/components/TextInput.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const TextInput: React.FC<Props> = () => {
     return (
         <div>
-            <input className="focus:bg-gray-50"/>
+            <input className="bg-gray-100 focus:bg-gray-50 border-l-8 pl-4" placeholder="type"/>
         </div>
     );
 }
